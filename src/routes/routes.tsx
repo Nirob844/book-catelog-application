cd726c517fcbd57010fbb9e7ca1f5aee094a8793
Nirob Hasan <nirob427417@gmail.com>
2023-07-13 22:37:08 +0600

    navbar & footer

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Register/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
